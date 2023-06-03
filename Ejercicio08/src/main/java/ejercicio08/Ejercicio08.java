dd39e06704a129ab17c1f458d0c9dc0bb27be90e
Sevillegas <sevillegas@unal.edu.co>
2023-06-02 19:43:08 -0500

    Ejercicio8.1
    Paquete swing

---
+++ Ejercicio08/src/main/java/ejercicio08/Ejercicio08.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicio08;
+
+
+public class Ejercicio08 {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+     
+               
+    }
+}
